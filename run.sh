f5b55c88edb581ee09cf6a135daef8ee5e63b61e
Andrew Rich <andrew.rich@gmail.com>
2024-06-26 14:17:24 -0700

    better env var names

--- run.sh
+++ run.sh
@@ -4,9 +4,9 @@
 env_vars=$(env | grep '^PR_REPORT_' | awk -F= '{print "-e", $1}')
 
 # Use command line arguments if provided, otherwise use environment variables
-TOKEN_ARG=${GITHUB_TOKEN:+"--token=$PR_REPORT_TOKEN"}
-REPO_ARG=${GITHUB_REPO:+"--repo=$PR_REPORT_REPO"}
-DAYS_ARG=${DAYS_AGO:+"--days=$PR_REPORT_DAYS_AGO"}
+TOKEN_ARG=${PR_REPORT_DAYS_AGO:+"--token=$PR_REPORT_DAYS_AGO"}
+REPO_ARG=${PR_REPORT_REPO:+"--repo=$PR_REPORT_REPO"}
+DAYS_ARG=${PR_REPORT_DAYS_AGO:+"--days=$PR_REPORT_DAYS_AGO"}
 
 # Run the Docker container, passing in the environment variables
 docker run --rm $env_vars -v "$(pwd)/.env:/app/.env" github-pr-report $TOKEN_ARG $REPO_ARG $DAYS_ARG
